d582640f941a4fb38f3e7d5490cac0bb87e57290
tzerk <XnIASwkTCBlGgudcCsml>
2015-03-16 15:49:12 +0100

    attr argument to add tooltips to any element where attr=refId

--- R/tooltip.R
+++ R/tooltip.R
@@ -4,6 +4,7 @@
 #' 
 #' @param refId \code{\link{character}} (required): id of the element the tooltip is to be attached to.
 #' @param text \code{\link{character}}: Text to be displayed in the tooltip.
+#' @param attr \code{\link{character}}: Attach tooltip to all elements with attribute \code{attr='refId'}.
 #' @param animation \code{\link{logical}}: Apply a CSS fade transition to the tooltip.
 #' @param delay \code{\link{numeric}}: Delay showing and hiding the tooltip (ms).
 #' @param html \code{\link{logical}}: Insert HTML into the tooltip.
@@ -12,7 +13,7 @@
 #' 
 #' @examples 
 #' # html code
-#' tt <- tooltip("someId", "This is a tooltip.")
+#' tt <- tooltip("elementId", "This is a tooltip.")
 #' str(tt)
 #' 
 #' # example app
@@ -33,8 +34,8 @@
 #'                                  "c" = "c")),
 #'   tooltip("cboxg", "This is a <b>checkbox group</b>", html = TRUE),
 #'   
-#'   textInput("textIn", "Textinput"),
-#'   tooltip("textIn", "This is a text input field"),
+#'   selectInput("select", "Selectinput", selected = "a", choices = c("a"="a", "b"="b")),
+#'   tooltip("select", "This is a text input field", attr = "for", placement = "ri),
 #'   
 #'   passwordInput("pwIn", "Passwordinput"),
 #'   tooltip("pwIn", "This is a password input field"),
@@ -49,9 +50,14 @@
 #' }
 #' @import shiny
 #' @export
-tooltip <- function(refId, text, animation = TRUE, delay = 100, html = TRUE, placement = 'auto', trigger = 'hover') {
-  tagList(        
-    tags$head(tags$script(sprintf("$(window).load(function(){ $('#%s').tooltip({ html: %s, trigger: '%s', title: '%s', animation: %s, delay: {'show': %i, 'hide': %i}, placement: '%s' }); })",
-                                  refId, tolower(html), trigger, text, tolower(animation), delay, delay, placement)))
+tooltip <- function(refId, text, attr = NULL, animation = TRUE, delay = 100, html = TRUE, placement = 'auto', trigger = 'hover') {
+  if (is.null(attr))
+    el <- sprintf("'#%s'", refId)
+  else
+    el <- sprintf("\"[%s='%s']\"", attr, refId)
+  
+  tagList(
+      tags$head(tags$script(HTML(sprintf("$(window).load(function(){ $(%s).tooltip({ html: %s, trigger: '%s', title: '%s', animation: %s, delay: {'show': %i, 'hide': %i}, placement: '%s' }); })",
+                                  el, tolower(html), trigger, text, tolower(animation), delay, delay, placement))))
   )
 }
