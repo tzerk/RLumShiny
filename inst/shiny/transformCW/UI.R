6b0820f48ec4dd77e14d48e69a686c3db714ca74
tzerk <XnIASwkTCBlGgudcCsml>
2017-03-01 15:55:03 +0100

    Add text/csv to file input to support Safari (fixes #16)

--- inst/shiny/transformCW/UI.R
+++ inst/shiny/transformCW/UI.R
@@ -17,7 +17,7 @@ function(request) {
                                         # file upload button (data set 1)
                                         fileInput(inputId = "file", 
                                                   label = strong("Primary data set"),
-                                                  accept="text/plain, .csv"),
+                                                  accept="text/plain, .csv, text/csv"),
                                         tooltip(refId = "file1", text = tags$img(src='file_structure.png', width='250px')),
                                         # rhandsontable input/output
                                         fluidRow(
