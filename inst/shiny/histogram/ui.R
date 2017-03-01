76b27730ff1e1ade2015aa13368a4a90b12c104b
tzerk <XnIASwkTCBlGgudcCsml>
2017-03-01 15:48:45 +0100

    Add text/csv to file input

--- inst/shiny/histogram/ui.R
+++ inst/shiny/histogram/ui.R
@@ -13,7 +13,7 @@ function(request) {
                                         # file upload button (data set 1)
                                         fileInput(inputId = "file1", 
                                                   label = strong("Primary data set"),
-                                                  accept="text/plain, .csv"),
+                                                  accept="text/plain, .csv, text/csv"),
                                         tooltip(refId = "file1", text = tags$img(src='file_structure.png', width='250px')),
                                         # rhandsontable input/output
                                         fluidRow(
