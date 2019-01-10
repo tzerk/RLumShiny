668fd9b2a32a04e8f02a46afc5f51d0356d7efa9
Tzerk <christoph.burow@gmx.net>
2019-01-10 09:27:06 +0100

    gamma: check min version requirement of 'Luminescence'
    A workaround to publish the scale_GammaDose() application before the function itself, which will be introduced in v0.9.0 of 'Luminescence'

--- inst/shiny/scalegamma/global.R
+++ inst/shiny/scalegamma/global.R
@@ -5,6 +5,15 @@ library(shiny)
 library(data.table)
 library(rhandsontable)
 
+if (utils::packageVersion("Luminescence") < "0.9.0")
+  stop(
+    "\n\n", rep("#", 30), "\n",
+    "This application requires 'Luminescence' version >=0.9.0.\n",
+    "See ?Luminescence::install_DevelopmentVersion() to get the ",
+    "latest version of the package.",
+    "\n", rep("#", 30), "\n\n",
+    call. = FALSE)
+
 data("ExampleData.ScaleGammaDose")
 
 example_data <- ExampleData.ScaleGammaDose
