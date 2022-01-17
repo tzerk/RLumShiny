f3bd84b1482d55ed7745475d5f8fed8c8893f7e9
RLumSK <sebastian.kreutzer@aber.ac.uk>
2022-01-17 12:38:19 +0000

    Fix error in scalegamma

--- inst/shiny/scalegamma/ui.R
+++ inst/shiny/scalegamma/ui.R
@@ -11,11 +11,11 @@ function(request) {
                    tabsetPanel(id = "tabs", type = "pill", selected = "Data",
                                # Tab 1: Data input
                                tabPanel("Data",
-                                        
+
                                         # informational text
                                         div(align = "center", h5("Data upload")),
                                         # file upload button (data set 1)
-                                        fileInput(inputId = "file", 
+                                        fileInput(inputId = "file",
                                                   label = strong("Primary data set"),
                                                   accept="text/plain, .csv, text/csv"),
                                         # rhandsontable input/output
@@ -25,29 +25,29 @@ function(request) {
                                           "Only one value in 'Sample offset (cm)' allowed, which indicates the position of the sample in a layer, measured from the bottom of respective layer.",
                                           "<br>Right-click on the table to add or remove rows. Copy-paste is supported.")
                                         ))
-                                        
+
                                ),##EndOf::Tab_1
-                               
+
                                tabPanel("Settings",
                                         tags$br(),
-                                        selectInput(inputId = "frac_dose", "Fractional gamma dose table", 
+                                        selectInput(inputId = "frac_dose", "Fractional gamma dose table",
                                                     choices = c("Aitken 1985" = "Aitken1985")
                                         ),
-                                        selectInput(inputId = "conv_fac", "Conversion Factors", 
+                                        selectInput(inputId = "conv_fac", "Conversion Factors",
                                                     choices = c(
-                                                      "Cresswell et al. 2019" = "Cresswelletal2019",
+                                                      "Cresswell et al. 2018" = "Cresswelletal2018",
                                                       "Liritzis et al. 2013" = "Liritzisetal2013",
                                                       "Guerin et al. 2011" = "Guerinetal2011",
                                                       "Adamiec & Aitken 1998" = "AdamiecAitken1998"
                                                     )
                                         )
                                ),
-                               
+
                                RLumShiny:::exportTab("export", filename = "scalegammadose"),
                                RLumShiny:::aboutTab("about", "scalegammadose")
                    )##EndOf::tabsetPanel
       ),##EndOf::sidebarPanel
-      
+
       # 3 - output panel
       mainPanel(width = 7,
                 # insert css code inside <head></head> of the generated HTML file:
