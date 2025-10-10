aac0e9a3f3246228c93637c22351dcabf34d881b
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-10-10 14:22:46 +0200

    Add a way to import a .psl file for the portableOSL app.
    This adds an initial version of the importTab module.

---
+++ R/module_importTab.R
@@ -0,0 +1,20 @@
+importTab <- function(id, label, accept) {
+
+  ## create a namespace function using the provided id
+  ns <- NS(id)
+
+  tabPanel("Import",
+
+           div(align = "center", h5("Data import")),
+
+           # file upload button
+           fileInput(inputId = "file",
+                     label = strong(label),
+                     accept = accept),
+
+           # import
+           actionButton(inputId = "import",
+                        label = "Import",
+                        class = "btn btn-success")
+           )
+}
