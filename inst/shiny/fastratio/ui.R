3c72023bde396fff9a10ceaa738aaba823a89f2c
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-10-29 15:29:53 +0100

    Use the importTab module in the fastratio app.
    Now the fastratio app can import an XSYG file, although at the moment no
    curve selection available, so the first curve is hardcoded.
    
    This expands the importTab module letting it accept a callback function,
    through which additional UI elements can be added by the apps.

--- inst/shiny/fastratio/ui.R
+++ inst/shiny/fastratio/ui.R
@@ -8,27 +8,22 @@ function(request) {
       # elements
       sidebarPanel(width = 5,
                    # include a tabs in the input panel for easier navigation
-                   tabsetPanel(id = "tabs", type = "pill", selected = "Data",
+                   tabsetPanel(id = "tabs", type = "pill", selected = "Import",
                                # Tab 1: Data input
-                               tabPanel("Data",
+                               RLumShiny:::importTab("import",
+                                                     "XSYG file (.xsyg) or CSV file with two columns (Time and Counts)",
+                                                     "application/xml, .xsyg, text/csv, .csv",
+                                                     callback = function() {
+                                                       # rhandsontable input/output
+                                                       fluidRow(
+                                                           column(width = 6,
+                                                                  rHandsontableOutput(outputId = "table_in_primary")
+                                                                  ),
+                                                           column(width = 6)
+                                                       )
+                                                     }
+                               ), ## EndOf::Tab_1
 
-                                        # informational text
-                                        div(align = "center", h5("Data upload")),
-                                        # file upload button (data set 1)
-                                        fileInput(inputId = "file",
-                                                  label = strong("Primary data set"),
-                                                  placeholder = "A CSV file with two columns (Time and Counts)",
-                                                  accept="text/plain, .csv, text/csv"),
-                                        # rhandsontable input/output
-                                        fluidRow(
-                                          column(width = 6,
-                                                 rHandsontableOutput(outputId = "table_in_primary")
-                                          ),
-                                          column(width = 6)
-                                        )
-                                        
-                               ),##EndOf::Tab_1
-                               
                                tabPanel("Method",
                                         div(align = "center", h5("Input data preprocessing")),
                                         sliderInput(inputId = "deadchannels", "Dead channels", value = c(1, 1000), min = 1, max = 1000, step = 1, 
