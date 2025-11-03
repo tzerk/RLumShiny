tzerk/RLumShiny

commit 38341c65d4f52c46f1d8ef4349cb9ce59756202f

Add indentation to the printCode() output.

R/module_printCode.R
@@ -63,35 +63,35 @@ printCode <- function(input, output, session, ...,
 
 ## Format a complete function call
 .format_function_call <- function(fun) {
-  rets <- if (!is.null(fun$rets)) paste(fun$rets, "<- ") else ""
-  if (!is.null(fun$info))
-    rets <- paste0("# ", fun$info, "\n", rets)
-  sprintf("%s%s(%s)\n", rets, fun$name, .format_args(fun$arg1, fun$args))
-}
+  rets <- if (!is.null(fun$rets)) paste(fun$rets, "<-", fun$name) else fun$name
+  len.indent <- nchar(rets) + 1 # for the opening bracket
+  indent.sep <- sprintf(",\n%s", strrep(" ", len.indent))
 
-## Format a list of arguments into a comma-separated string
-.format_args <- function(arg1, args) {
-  names <- names(args)
+  ## collect the argument names
+  names <- names(fun$args)
   if (is.null(names))
-    names <- rep(NA, length(args))
+    names <- rep(NA, length(fun$args))
   names[which(names == "")] <- NA
 
+  ## format a list of arguments into an indented comma-separated string
   args <- paste(mapply(function(name, arg) {
     if (inherits(arg, "character"))
       arg <- paste0("'", arg, "'")
     if (inherits(arg, "list"))
       arg <- deparse(arg)
     if (length(arg) > 1)
-      arg <- paste0("c(", paste(arg, collapse = ", "), ")")
+      arg <- paste0("c(", paste(arg, collapse = ", "))
     if (is.null(arg))
       arg <- "NULL"
     if (!is.na(name))
       paste(name, "=", arg)
     else
       arg
-  }, names[-1], args[-1]), collapse = ",\n")
+  }, names[-1], fun$args[-1]), collapse = indent.sep)
 
-  if (is.null(arg1))
-    return(args)
-  paste(arg1, args, sep = ",\n")
+  if (!is.null(fun$info))
+    rets <- paste0("# ", fun$info, "\n", rets)
+  if (!is.null(fun$arg1))
+    args <- paste(fun$arg1, args, sep = indent.sep)
+  sprintf("%s(%s)\n", rets, args)
 }

commit d2513f2f78cf92270b648b29bd6839dc1d1a783a

Fix printing of vectors in printCode().
This was a regression from 38341c65.

R/module_printCode.R
@@ -80,7 +80,7 @@ printCode <- function(input, output, session, ...,
     if (inherits(arg, "list"))
       arg <- deparse(arg)
     if (length(arg) > 1)
-      arg <- paste0("c(", paste(arg, collapse = ", "))
+      arg <- paste0("c(", paste(arg, collapse = ", "), ")")
     if (is.null(arg))
       arg <- "NULL"
     if (!is.na(name))