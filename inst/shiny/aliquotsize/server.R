aacf96f8d3e13d2d9022354f7fc6744ced723e92
Marco Colombo <marco.colombo@uni-heidelberg.de>
2025-11-03 12:30:03 +0100

    Fix printing of R code from the aliquotsize app.
    This was a regression from 23586f0a.

--- inst/shiny/aliquotsize/server.R
+++ inst/shiny/aliquotsize/server.R
@@ -92,7 +92,8 @@ function(input, output, session) {
     code.output <- callModule(RLumShiny:::printCode, "printCode",
                               n_inputs = 0,
                               list(name = "calc_AliquotSize",
-                                   args = values$args))
+                                   args = c(dummy = NA, # the first argument is removed by printCode()
+                                            values$args)))
 
     output$plotCode<- renderText({
       code.output
