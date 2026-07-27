9c18afd321bd118518cac7cfc63db224bd38896d
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-07-27 16:18:15 +0200

    Update column names in the sarCWOSL app.

--- inst/shiny/sarCWOSL/server.R
+++ inst/shiny/sarCWOSL/server.R
@@ -209,8 +209,12 @@ function(input, output, session) {
 
     if (onlyHighlights) {
       ## remove columns for secondary model parameters
-      rm.idx <- match(c("D01", "D01.ERROR", "D02", "D02.ERROR", "R", "R.ERROR",
-                        "Dc", "D63", "HPDI68_L", "HPDI68_U", "HPDI95_L", "HPDI95_U",
+      rm.idx <- match(c("D01", "D01.ERROR", "D02", "D02.ERROR",
+                        "R", "R.LOWER", "R.UPPER",
+                        "Dc", "Dc.LOWER", "Dc.UPPER",
+                        "D63", "D63.LOWER", "D63.UPPER",
+                        "D80", "D80.LOWER", "D80.UPPER",
+                        "HPDI68_L", "HPDI68_U", "HPDI95_L", "HPDI95_U",
                         "signal.range", "background.range",
                         "signal.range.Tx", "background.range.Tx", "UID"),
                       colnames(data))
