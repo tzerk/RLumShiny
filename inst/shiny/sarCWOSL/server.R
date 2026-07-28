80a94a3c4943c0da64de399aca5dd01f03dda713
Marco Colombo <marco.colombo@uni-heidelberg.de>
2026-07-28 15:06:54 +0200

    Ensure the sarCWOSL results are stored in the correct index.
    When clicking on "Analyze all", we need to match the position number to
    the vector of all available positions, otherwise we may store the result
    into the wrong index. For example, if our input contains two aliquots for
    positions 1 and 17, the second should be stored at index 2, not 17.

--- inst/shiny/sarCWOSL/server.R
+++ inst/shiny/sarCWOSL/server.R
@@ -145,10 +145,10 @@ function(input, output, session) {
   })
 
   output$positions <- renderUI({
-    positions <- sort(RLumShiny:::get_unique_positions(values$data_primary))
+    values$all_positions <- RLumShiny:::get_unique_positions(values$data_primary)
     radioButtons("positions", "Positions",
-                 choiceNames = positions,
-                 choiceValues = seq_along(positions),
+                 choiceNames = values$all_positions,
+                 choiceValues = seq_along(values$all_positions),
                  selected = 1,
                  inline = TRUE)
   })
@@ -162,7 +162,13 @@ function(input, output, session) {
 
   output$curves <- renderUI({
     req(input$positions)
-    choices <- seq_along(values$data_primary[[as.integer(input$positions)]]@records)
+    pos <- as.integer(input$positions)
+
+    ## avoid indexing at an inexistent position when the input file changes
+    if (pos > length(values$data_primary))
+      return()
+
+    choices <- seq_along(values$data_primary[[pos]]@records)
     data <- values$data_filtered %||% values$data_primary
     uids <- get_uids(data[[1]])
     checkboxGroupInput("curves", "Curves",
@@ -183,7 +189,8 @@ function(input, output, session) {
     if (inherits(results, "RLum.Results")) {
       for (pos in results$data$POS) {
         if (is.na(pos)) next()
-        values$results[[pos]] <- results$data[results$data$POS == pos, ]
+        idx <- match(pos, values$all_positions)
+        values$results[[idx]] <- results$data[results$data$POS == pos, ]
       }
     }
 
@@ -199,8 +206,12 @@ function(input, output, session) {
     results <- RLumShiny:::tryNotify(do.call(analyse_SAR.CWOSL, values$args))
 
     ## store the results obtained for this position
-    if (inherits(results, "RLum.Results"))
-      isolate(values$results[[input$positions]] <- results$data)
+    if (inherits(results, "RLum.Results")) {
+      idx <- match(results@data$data$POS, values$all_positions)
+      if (!is.na(idx)) {
+        isolate(values$results[[idx]] <- results$data)
+      }
+    }
   })
 
   getResultsTable <- function(onlyHighlights = FALSE) {
